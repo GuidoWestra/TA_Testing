a5fb90115f592b145372287f6d65b9aaaf0bee97
Guido Westra <westra.guido@gmail.com>
2021-02-14 13:10:38 +0100

    Added Alphabet Print

---
+++ Squares/index.js
@@ -0,0 +1,21 @@
+function Buildhouse() {
+  //Drawing Variables
+  let a;
+  let b;
+
+  // X & Y, Axis defined here!
+  const x = 5;
+  const y = 5;
+
+  /*Intended output: 
+
+  *****
+  *   *
+  *   *
+  *   *
+  *****
+
+
+  */
+  return ``;
+}
