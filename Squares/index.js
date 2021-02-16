a11bbc7ded04ca956c59452d47e2e3554120978c
Guido Westra <westra.guido@gmail.com>
2021-02-16 14:55:46 +0100

    Working on House builder

--- Squares/index.js
+++ Squares/index.js
@@ -1,21 +1,23 @@
 function Buildhouse() {
   //Drawing Variables
-  let a;
-  let b;
+  let a = 0;
+  let b = 0;
 
   // X & Y, Axis defined here!
-  const x = 5;
+  const x = 8;
   const y = 5;
-
-  /*Intended output: 
-
-  *****
-  *   *
-  *   *
-  *   *
-  *****
-
-
-  */
-  return ``;
+  console.log(`Details entered X:${x} Y:${y}`);
+  while (a < x) {
+    if (y === 5) {
+      console.log("*   *");
+    }
+    if (y === 4) {
+      console.log("****");
+    }
+    if (y === 3) {
+      console.log("*****");
+    }
+    a++;
+  }
 }
+Buildhouse();
--- TSFizzBuzz/index.ts
+++ TSFizzBuzz/index.ts
@@ -4,11 +4,11 @@ while (count < 100) {
   if (count === 0) {
     console.log(count);
   } else if (count % 15 === 0) {
-    console.log("FizzBuzz");
+    console.log("FizzBuzz", count);
   } else if (count % 5 === 0) {
-    console.log("Buzz");
+    console.log("Buzz", count);
   } else if (count % 3 === 0) {
-    console.log("Fizz");
+    console.log("Fizz", count);
   } else console.log(count);
   count++;
 }
