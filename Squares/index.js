28afa752773c72fa50cf1d8a62226a2639b1fcc9
Guido Westra <westra.guido@gmail.com>
2021-02-16 15:23:45 +0100

    Square is printing

--- Squares/index.js
+++ Squares/index.js
@@ -1,22 +1,26 @@
 function Buildhouse() {
-  //Drawing Variables
+  let params = process.argv.slice(2);
+
+  // Drawing axis
   let a = 0;
   let b = 0;
+  let Line = "";
 
   // X & Y, Axis defined here!
-  const x = 8;
-  const y = 5;
+  let x = params[0];
+  let y = params[1];
+
+  // Providing details to User
   console.log(`Details entered X:${x} Y:${y}`);
+
+  // House Building
+
   while (a < x) {
-    if (y === 5) {
-      console.log("*   *");
+    while (b < y) {
+      Line = `${Line}*`;
+      b++;
     }
-    if (y === 4) {
-      console.log("****");
-    }
-    if (y === 3) {
-      console.log("*****");
-    }
+    console.log(Line);
     a++;
   }
 }
